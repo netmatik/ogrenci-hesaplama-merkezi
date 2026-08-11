f9d69f2541ec7e1d90ecbe21bcbb82c005d8359d
netmatik <uzunseymanur06@gmail.com>
2026-08-11 21:13:10 +0300

    Update universiteler.js

--- universiteler.js
+++ universiteler.js
@@ -1,1 +1,15 @@
-const programVerileri = [];
+const programVerileri = [
+    {
+        universite: "ÖRNEK - VERİ SİSTEMİ",
+        bolum: "Yönetim Bilişim Sistemleri",
+        tur: "devlet",
+        puan: "EA",
+        sehir: "Türkiye",
+        ogretim: "Örgün",
+        burs: "Devlet",
+        siralama2025: null,
+        siralama2024: null,
+        siralama2023: null
+    }
+
+];

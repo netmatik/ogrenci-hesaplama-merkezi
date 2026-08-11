85b5755cf55eda232471079856f0a86b5ea15024
netmatik <uzunseymanur06@gmail.com>
2026-08-11 19:05:08 +0300

    Create universiteler.js

---
+++ universiteler.js
@@ -0,0 +1,23 @@
+const universiteler = [
+    {
+        universite: "Avrasya Üniversitesi",
+        sehir: "Trabzon",
+        bolum: "Yönetim Bilişim Sistemleri",
+        puanTuru: "EA",
+        burs: "Burslu",
+
+        siralama: {
+            2023: null,
+            2024: null,
+            2025: null,
+            2026: null
+        },
+
+        puan: {
+            2023: null,
+            2024: null,
+            2025: null,
+            2026: null
+        }
+    }
+];
